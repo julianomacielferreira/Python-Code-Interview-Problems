d003a507be023f6ea2b91ac5c66e54e4642f0a28
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2025-08-26 19:29:14 -0300

    Add unit tests for the Topological Sorting algorithm.

---
+++ problem-7/test_course_schedule_v2.py
@@ -0,0 +1,45 @@
+"""
+The MIT License
+
+Copyright 2025 Juliano Maciel.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import unittest
+from course_schedule_v2 import Graph
+
+
+class GraphTestCase(unittest.TestCase):
+    def test_init(self):
+        graph = Graph()
+
+        self.assertEqual(graph.graph, {})
+        self.assertEqual(graph.in_degree, {})
+
+    def test_add_edge(self):
+        graph = Graph()
+
+        graph.add_edge('A', 'B')
+
+        self.assertEqual(graph.graph, {'A': ['B']})
+        self.assertEqual(graph.in_degree, {'A': 0, 'B': 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
